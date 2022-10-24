da5646feea144c9e2c2614d407f447b8ddc25ff3
Minion3468 <100939401+Minion3468@users.noreply.github.com>
2022-10-24 15:15:39 +0200

    Update Form1.cs

--- WinFormsApp2/Form1.cs
+++ WinFormsApp2/Form1.cs
@@ -23,21 +23,46 @@ namespace WinFormsApp2
         {
             int maxX = this.Size.Width - button1.Size.Width - 12;
             int maxY = this.Size.Height - button1.Size.Height - 12;
+            toolStripStatusLabel1.Text = "punkty " + punkty.ToString();
             Point o = new Point();
             o.X = l.Next(0, maxX);
             o.Y = l.Next(12, maxY);
             button1.Location = o;
-            timer1.Start();
+            timer1.Stop();
+            timer1.Start();    
+            punkty++;
+            
+            
+            Color kolorT = Color.FromArgb(l.Next(256) , l.Next(256), l.Next(256));
+            this.BackColor = kolorT;
+            button1.BackColor = kolorT;
+
+
+
 
         }
+        
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            
-           
+            int maxX = this.Size.Width - button1.Size.Width - 12;
+            int maxY = this.Size.Height - button1.Size.Height - 12;
+            toolStripStatusLabel1.Text = "punkty " + punkty.ToString();
+            Point o = new Point();
+            o.X = l.Next(0, maxX);
+            o.Y = l.Next(12, maxY);
+            button1.Location = o;
+            punkty = 0;
+
+
         }
 
         private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
         {
 
         }
